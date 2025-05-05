3e48a03613bbc67583231d7a6b61c3bdf3745494
Fahad Bhuiyan <mjlickerlim56789@gmail.com>
2025-05-05 17:03:35 +0600

    fix: border separator border fixed

--- resources/js/components/border-separator.tsx
+++ resources/js/components/border-separator.tsx
@@ -7,9 +7,8 @@ const BorderSepartor = ({ className }: BorderSepartorProps) => {
     return (
         <div>
             <div className={cn('border-y ', className)}>
-                <div className="w-full bg-[repeating-linear-gradient(-45deg,var(--color-border),var(--color-border)_1px,transparent_1px,transparent_6px)] py-6 opacity-35">
-                    <div className="container border-x z-30">
-
+                <div className="w-full bg-[repeating-linear-gradient(-45deg,var(--color-border),var(--color-border)_1px,transparent_1px,transparent_6px)]  opacity-35">
+                    <div className="container border-x !opacity-100 py-6">
                     </div>
                 </div>
             </div>
