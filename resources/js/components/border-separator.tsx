8e922fe74e6dcf501ac76747badae637c8ed34f3
Fahad Bhuiyan <78530442+eXeis-ixt@users.noreply.github.com>
2025-09-14 15:58:33 +0600

    Update border-separator.tsx

--- resources/js/components/border-separator.tsx
+++ resources/js/components/border-separator.tsx
@@ -5,13 +5,9 @@ interface BorderSepartorProps {
 }
 const BorderSepartor = ({ className }: BorderSepartorProps) => {
     return (
-        <div>
-            <div className={cn('border-y ', className)}>
-                <div className="w-full bg-[repeating-linear-gradient(-45deg,var(--color-border),var(--color-border)_1px,transparent_1px,transparent_6px)]  opacity-35">
-                    <div className="container border-x !opacity-100 py-6">
-                    </div>
-                </div>
-            </div>
+        <div className={cn('relative border-y', className)}>
+            <div className="absolute inset-0 bg-[repeating-linear-gradient(-45deg,var(--color-border),var(--color-border)_1px,transparent_1px,transparent_6px)] opacity-35" />
+            <div className="container relative border-x py-6" />
         </div>
     );
 };
